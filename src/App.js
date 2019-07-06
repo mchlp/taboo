e8a116380019da5e9b0fc4c2a84fb0f9957530ef
Shirley Zhang <zhangshirleyemail@gmail.com>
2019-07-05 22:56:53 -0400

    edited AddTeam

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Game from './Components/Game';
+import AddTeam from './Components/AddTeam';
 import backend from './backend';
 
 function App() {
     const words = backend.getWord();
     return (
         <div className="App">
-            <Game word={words[0]} restricted={words.slice(1)}/>  {/* testing!*/}
+            <AddTeam />             {/* testing!*/ }
         </div>
     );
 }
 
 export default App;
+//<Game word={words[0]} restricted={words.slice(1)}/>
--- src/Components/AddTeam.js
+++ src/Components/AddTeam.js
@@ -1,17 +1,75 @@
 import React, {Component} from 'react';
-import {Button} from 'react-bootstrap';
-import 'Container.css';
+import { Button, Form, Row, Container, FormGroup, Col} from 'react-bootstrap';
+import './Container.css';
+import './AddTeam.css';
 
-class TeamButton extends Component{
+var teamNames = [];
+
+class AddTeamButton extends Component{
+    constructor(props){
+        super(props);
+
+        this.state ={
+            value: ''
+        };
+        this.handleClick = this.handleClick.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    
+
+    handleClick(event){
+        
+        teamNames.push(event.target.value);
+        console.log(this.state.value);
+
+    }
+    
+    handleSubmit(event){
+        this.setState({value: event.target.value});
+
+    }
+    render(){
+        return(
+            <Container>
+                <FormGroup>
+                    <Row>
+                        <Col>
+                            <Form.Control onChange={this.handleSubmit} type="text" value={this.state.value} placeholder="Team Name">
+                            </Form.Control>
+                        </Col>
+                        <Col>
+                            <Button type="submit" onClick={this.handleClick}>Add Team</Button>
+                        </Col>
+                    </Row>
+                </FormGroup>
+            </Container>
+        );
+    } 
+}
+
+class Teams extends Component{
     constructor(props){
         super(props);
     }
-
+  
     render(){
+
+        let renderedTeamNames = [];
+
+        for(let i = 0; i < teamNames.length; i++){
+            renderedTeamNames.push(
+                <Row>
+                    <Button>
+                        {teamNames[i]} 
+                    </Button>
+                </Row>
+            );
+        }
         return(
-            <Button>
-                {this.props.text};
-            </Button>
+            <div>
+                {renderedTeamNames}
+            </div>
         );
     }
 }
@@ -23,8 +81,8 @@ class AddTeam extends Component{
             <body>
                 <Container>
                     <h1 className="Title" id="TeamsText">Teams!</h1>
-                        
-                        
+                    <AddTeamButton />
+                    <Teams/>
                     
 
                 </Container>
