f0d25956aa313334f5bc6667231469925c173e02
Michael Pu <michael.pu123@gmail.com>
2019-07-15 03:56:25 -0400

    First deployment

---
+++ precache-manifest.0a6f173086c3d64d1c61cdf0d705b306.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c95090b6991ac3335a0516d739d597a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2469f5544c19e338e53",
+    "url": "/static/css/main.e3b6e47c.chunk.css"
+  },
+  {
+    "revision": "b005d713b69c1c47b0ea",
+    "url": "/static/js/2.32827277.chunk.js"
+  },
+  {
+    "revision": "a2469f5544c19e338e53",
+    "url": "/static/js/main.9fda875e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
