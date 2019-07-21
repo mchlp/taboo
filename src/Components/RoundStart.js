6daf69c3350caf2043937e102bd4320ce9a6c509
Shirley Zhang <zhangshirleyemail@gmail.com>
2019-07-21 18:45:03 -0400

    styling for scoreboard

--- src/Components/RoundStart.js
+++ src/Components/RoundStart.js
@@ -3,6 +3,8 @@ import Scoreboard from './Scoreboard';
 import backend from '../backend';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import './Scoreboard.css';
 
 export default class RoundStart extends Component {
     render() {
@@ -13,12 +15,12 @@ export default class RoundStart extends Component {
         const teamTurn = backend.getTeamNames()[backend.getCurrentTeam()];
 
         return (
-            <div>
-                <h1>Ready to start round?</h1>
-                <h2>It's {teamTurn}'s turn!</h2>
+            <Container id="scoreboard-container">
+                <h4 id="start-round-title">Ready to start round?</h4>
+                <h2 id="team-turn-text">It's {teamTurn}'s turn!</h2>
                 <Scoreboard />
-                <Link to='/play'><Button>Ready</Button></Link>
-            </div>
+                <Link to='/play'><Button className="turquoise-button" id="ready-button">Ready</Button></Link>
+            </Container>
         );
     }
 }
--- src/Components/Scoreboard.js
+++ src/Components/Scoreboard.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import backend from '../backend';
 import { withRouter } from 'react-router-dom';
+import {Container, Row, Col} from 'react-bootstrap';
+import './Container.css';
+import './Scoreboard.css';
 
 class Scoreboard extends Component {
     render() {
@@ -25,22 +28,37 @@ class Scoreboard extends Component {
                 }
             }
             teamData.push(
-                <div key={i}>
-                    <p>{backend.getTeamNames()[i]}</p>
-                    <p>Score: {backend.getScores()[i]}</p>
-                </div>
+                <Row key={ i } className="holder">
+                    <Col><h4 className="scoreboard-text" id="rank-text">{ i + 1 }</h4></Col>
+                    <Col xs={6}>
+                        <h4 className="scoreboard-text">{backend.getTeamNames()[i]}</h4>
+                    </Col>
+                    <Col>
+                        <h4 className="scoreboard-text">{backend.getScores()[i]}</h4>
+                    </Col>
+                </Row>
             );
         }
 
         console.log(teamData);
 
         return (
-            <div>
-                <h1 style={{ color: 'black' }}>Scoreboard!</h1>
-                <p>Cap Points: {capPoints}</p>
+            <Container className="wrapper">
+                <h1 id="scoreboard-title">Scoreboard</h1>
+                <p id="cap-points-text">Cap Points: {capPoints}</p>
                 <h2>{teamWon ? winningTeam + ' won!' : null}</h2>
+
+                <Row id="column-header-text">
+                    <Col> </Col>
+                    <Col>
+                        <h3>Team</h3>
+                    </Col>
+                    <Col>
+                        <h3>Score</h3>
+                    </Col>
+                </Row>
                 {teamData}
-            </div>
+            </Container>
         );
     }
 }
